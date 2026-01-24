ed31ee9010584aabfbc2ac765abf005a230d286a
Fantasy <shidifuse@outlook.com>
2026-01-24 12:30:36 +0800

    Initial commit

---
+++ src/main/java/com/fantasy/fm/FantasyMusicApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyMusicApplication.class, args);
+    }
+
+}
