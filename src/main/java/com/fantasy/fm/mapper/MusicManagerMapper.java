1fe42fc54415d963f2d827d265a0a4b0f2bcb643
Fantasy <shidifuse@outlook.com>
2026-01-27 14:24:41 +0800

    重构MusicManagerMapper，使用LambdaQueryWrapper实现根据音乐ID查询音乐文件信息，移除原生SQL注解

--- src/main/java/com/fantasy/fm/mapper/MusicManagerMapper.java
+++ src/main/java/com/fantasy/fm/mapper/MusicManagerMapper.java
@@ -1,5 +1,6 @@
 package com.fantasy.fm.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fantasy.fm.domain.entity.MusicFileInfo;
 import org.apache.ibatis.annotations.Mapper;
@@ -8,6 +9,16 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface MusicManagerMapper extends BaseMapper<MusicFileInfo> {
 
-    @Select("select * from music_file_info where music_id = #{id}")
-    MusicFileInfo getFileInfoByMusicId(Long musicId);
+    /**
+     * 根据音乐ID查询音乐文件信息
+     *
+     * @param musicId 音乐ID
+     * @return 音乐文件信息
+     */
+    default MusicFileInfo getFileInfoByMusicId(Long musicId) {
+        return this.selectOne(
+                new LambdaQueryWrapper<MusicFileInfo>()
+                        .eq(MusicFileInfo::getMusicId, musicId)
+        );
+    }
 }
