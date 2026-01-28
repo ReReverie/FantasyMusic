ee69496a29ccb30096b6ecdb093c2fc9b7fe9ccd
Fantasy <shidifuse@outlook.com>
2026-01-28 20:04:46 +0800

    配置jwt配置文件

--- src/main/java/com/fantasy/fm/properties/JwtProperties.java
+++ src/main/java/com/fantasy/fm/properties/JwtProperties.java
@@ -1,8 +1,10 @@
 package com.fantasy.fm.properties;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Data
 @Component
 @ConfigurationProperties(prefix = "fm.jwt")
 public class JwtProperties {
