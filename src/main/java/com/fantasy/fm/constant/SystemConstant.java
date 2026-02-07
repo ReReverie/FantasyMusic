10a57dfc41be6242a97862fc1f226eb8188a2239
Fantasy <shidifuse@outlook.com>
2026-02-07 13:51:47 +0800

    添加音乐文件上传异常处理，更新音乐服务实现以在文件读取失败时删除OSS文件并抛出自定义异常

--- src/main/java/com/fantasy/fm/constant/SystemConstant.java
+++ src/main/java/com/fantasy/fm/constant/SystemConstant.java
@@ -2,6 +2,7 @@ package com.fantasy.fm.constant;
 
 public class SystemConstant {
     public static final String PERMISSION_DENIED = "当前用户权限不足,无法执行该操作!";
+    public static final String MUSIC_UPLOAD_FAILURE = "音乐文件可能出现了损坏,已取消上传文件: ";
     public static final String OSS_MUSIC_DIR = "music/";
     public static final String OSS_COVER_DIR = "cover/";
 }
---
+++ src/main/java/com/fantasy/fm/exception/MusicFileUploadException.java
@@ -0,0 +1,7 @@
+package com.fantasy.fm.exception;
+
+public class MusicFileUploadException extends BaseException {
+    public MusicFileUploadException(String message) {
+        super(message);
+    }
+}
